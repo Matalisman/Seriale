1116309049accf4e91833364d6f14f6ab7afc726
Matalisman <mateusz.borysewicz@interia.pl>
2016-11-07 00:38:10 +0100

    working hydrator wrong documentation handled

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -1,14 +1,25 @@
 <?php
 // Filename: /module/Blog/config/module.config.php
 return array(
+    'db' => array(
+        'driver'         => 'Pdo',
+        'username'       => 'root',  //edit this
+        'password'       => '',  //edit this
+        'dsn'            => 'mysql:dbname=seriale;host=localhost',
+        'driver_options' => array(
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        )
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
     ),
     'service_manager' => array(
-        'invokables' => array(
-            'Blog\Service\PostServiceInterface' => 'Blog\Service\PostService'
+        'factories' => array(
+            'Blog\Mapper\PostMapperInterface'   => 'Blog\Factory\ZendDbSqlMapperFactory',
+            'Blog\Service\PostServiceInterface' => 'Blog\Factory\PostServiceFactory',
+            'Zend\Db\Adapter\Adapter'           => 'Zend\Db\Adapter\AdapterServiceFactory'
         )
     ),
     'controllers'  => array(
---
+++ module/Blog/src/Blog/Mapper/Blog/Entity/PostInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mateusz
+ * Date: 07.11.2016
+ * Time: 00:33
+ */
+
+namespace Blog\Entity;
+
+
+class PostInterface
+{
+
+}
